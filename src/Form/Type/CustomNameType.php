49fc28827306364a6de00119ade0d3b419dd2816
khalil1608 <khalil1608@gmail.com>
2026-02-22 19:45:21 +0100

    Fix CustomNameType: extend AbstractType for Symfony form compatibility

--- src/Form/Type/CustomNameType.php
+++ src/Form/Type/CustomNameType.php
@@ -3,10 +3,11 @@
 namespace UbeeDev\AdminBundle\Form\Type;
 
 use UbeeDev\AdminBundle\Form\DataTransformer\NameTransformer;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType as SymfonyNameType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class CustomNameType
+class CustomNameType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
